ffb9f8f4db83f0811317c05c591c353238e66de6
Mankirat Singh <mankiratsingh2196@gmail.com>
2025-11-08 21:03:55 +0530

    Adding helper functions to simulation

--- include/simulation.hpp
+++ include/simulation.hpp
@@ -13,29 +13,77 @@ struct Simulation{
     s = 1e-4 * scale;
   }
 
-void step(double dt){
+  void addBody(Body body){
+    bodies.push_back(body);
+    for(int i = 0 ; i < bodies.size() ; ++i){
+      for(int j = i+1 ; j < bodies.size() ; ++j){
+        Vec2 distVec = bodies[j].pos - bodies[i].pos;
+        double dist = distVec.length();
+        Vec2 unitDist = distVec / dist; 
+        double force = G*(bodies[i].mass)*(bodies[j].mass) / (dist*dist + s*s);
+        Vec2 forceVec = unitDist * force;
+        bodies[i].applyForce(forceVec);
+        bodies[j].applyForce(-forceVec);
+      }
+    }
 
-  for(Body& body : bodies){
-    body.accPrev = body.acc;
-    body.pos = body.pos + (body.vel * dt + 0.5 * body.acc * dt * dt);
-  
+    for(Body& b : bodies){
+      b.accPrev = b.acc;
+    }
   
   }
-  for(Body& body : bodies){
-    body.resetAcc();
-  }
 
-  for(int i = 0 ; i < bodies.size() ; ++i){
-    for(int j = i+1 ; j < bodies.size() ; ++j){
-      Vec2 distVec = bodies[j].pos - bodies[i].pos;
-      double dist = distVec.length();
-      Vec2 unitDist = distVec / dist; 
-      double force = G*(bodies[i].mass)*(bodies[j].mass) / (dist*dist + s*s);
-      Vec2 forceVec = unitDist * force;
-      bodies[i].applyForce(forceVec);
-      bodies[j].applyForce(-forceVec);
+  void clear(){
+    time = 0;
+    while(!bodies.empty()){
+      bodies.pop_back();
     }
   }
+
+  const std::vector<Body>& getBodies() const {
+    return bodies;
+  }
+
+  double getEnergy() const {
+    double kinetic = 0;
+    double potential = 0;
+
+    for(Body body : bodies){
+      kinetic += 0.5*body.mass*body.vel.lengthSquared();
+    }
+
+    for(int i = 0 ; i < bodies.size() ; ++i){
+      for(int j = i+1 ; j < bodies.size() ; j++){
+        potential -= G*bodies[i].mass*bodies[j].mass / sqrt((bodies[i].pos - bodies[j].pos).lengthSquared() + s*s);
+      }
+    }
+
+    double totalEnergy = potential + kinetic;
+    return totalEnergy;
+  }
+
+  void step(double dt){
+
+    for(Body& body : bodies){
+      body.accPrev = body.acc;
+      body.pos = body.pos + (body.vel * dt + 0.5 * body.acc * dt * dt);
+    }
+
+    for(Body& body : bodies){
+      body.resetAcc();
+    }
+
+    for(int i = 0 ; i < bodies.size() ; ++i){
+      for(int j = i+1 ; j < bodies.size() ; ++j){
+        Vec2 distVec = bodies[j].pos - bodies[i].pos;
+        double dist = distVec.length();
+        Vec2 unitDist = distVec / dist; 
+        double force = G*(bodies[i].mass)*(bodies[j].mass) / (dist*dist + s*s);
+        Vec2 forceVec = unitDist * force;
+        bodies[i].applyForce(forceVec);
+        bodies[j].applyForce(-forceVec);
+      }
+    }
 
     for(Body& body : bodies){
       body.vel = body.vel + (0.5 * (body.accPrev + body.acc) * dt);
